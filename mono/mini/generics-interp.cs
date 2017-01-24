539f8aaf78dd8f07195339d4040494c5b9782b86
Bernhard Urban <bernhard.urban@xamarin.com>
2017-01-24 20:41:51 +0100

    generic-interp.cs: udpate

--- mono/mini/generics-interp.cs
+++ mono/mini/generics-interp.cs
@@ -16,19 +16,31 @@ class Tests
 	{
 		return Unbox<int> (1);
 	}
-	public static int test_1337_no_nullable_unbox ()
-	{
-		return Unbox<int> (1337);
-	}
-
-	public static int test_43981_nullable_unbox ()
-	{
-		return Unbox<int?> (0xabcd).Value;
-	}
 
 	public static int test_1_nullable_unbox_null ()
 	{
 		return Unbox<int?> (null).HasValue ? 0 : 1;
+	}
+
+	public static int test_1_nullable_box ()
+	{
+		return (int) Box<int?> (1);
+	}
+
+	public static int test_1_nullable_box_null ()
+	{
+		return Box<int?> (null) == null ? 1 : 0;
+	}
+
+	public static int test_1_isinst_nullable ()
+	{
+		object o = 1;
+		return (o is int?) ? 1 : 0;
+	}
+
+	static object Box<T> (T t)
+	{
+		return t;
 	}
 
 	static T Unbox <T> (object o) {
