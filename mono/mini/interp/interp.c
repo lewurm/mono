e03232650132cab10f62afc33b376654dec32185
Bernhard Urban-Forster <lewurm@gmail.com>
2020-02-10 09:49:56 +0100

    Revert "Significantly decrease nonrecursive interpreter memory use."
    This reverts commit 8c82ec8b331c82763e38bd637a16279d2a0fb8f2.

--- mono/mini/interp/interp.c
+++ mono/mini/interp/interp.c
@@ -131,11 +131,8 @@ add_frag (FrameStack *stack, int size)
 {
 	StackFragment *new_frag;
 
-	// FIXME: The behavior of an allocation not
-	// fitting in current chunk is very poor. Attempt
-	// instead a scheme managing one growable chunk
-	// by using multiple adjacent alloca.
-	int frag_size = 65536;
+	// FIXME:
+	int frag_size = 4096;
 	if (size + sizeof (StackFragment) > frag_size)
 		frag_size = size + sizeof (StackFragment);
 	new_frag = stack_frag_new (frag_size);
@@ -464,13 +461,9 @@ get_context (void)
 		/*
 		 * Use two stacks, one for InterpFrame structures, one for data.
 		 * This is useful because InterpFrame structures don't need to be GC tracked.
-		 * It also useful to fit allocations into iframe_stack perfectly.
-		 * FIXME: The behavior when allocations do not fit in current data_stack
-		 * chunk is very poor. Attempt a scheme with one growable chunk instead
-		 * using multiple adjacent alloca.
 		 */
-		frame_stack_init (&context->iframe_stack, 1024 * sizeof (InterpFrame));
-		frame_stack_init (&context->data_stack, 65536);
+		frame_stack_init (&context->iframe_stack, 8192);
+		frame_stack_init (&context->data_stack, 8192);
 		set_context (context);
 	}
 	return context;
